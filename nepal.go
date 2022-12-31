623c1badcedc17ee3ba45cf33579cdb829ad8727
ttmars <2292752593@qq.com>
2022-12-31 14:41:41 +0800

    add InitLog

--- nepal.go
+++ nepal.go
@@ -1,11 +1,15 @@
 package cob
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
+	"os"
 	"path"
 	"runtime"
 	"time"
@@ -17,7 +21,7 @@ type Nepal struct {
 var DNepal = Nepal{}
 
 // GetRandomString 获取n位随机字符串，n为偶数
-func (obj *Nepal)GetRandomString(n int) string {
+func (obj *Nepal)GetRandomString(n int) (result string) {
 	rand.Seed(time.Now().UnixNano())
 	randBytes := make([]byte, n/2)
 	rand.Read(randBytes)
@@ -25,7 +29,7 @@ func (obj *Nepal)GetRandomString(n int) string {
 }
 
 // GetRandomNum 获取N~M之间的随机整数，N<=M
-func (obj *Nepal)GetRandomNum(N,M int) int  {
+func (obj *Nepal)GetRandomNum(N,M int) (result int)  {
 	if M<N {
 		log.Fatalln("N必须小于等于M!")
 	}
@@ -59,8 +63,88 @@ func (obj *Nepal)CreateFileServer(localPath string, relativePath string, port st
 	}
 }
 
-// GetRootPath 获取项目根路径
+// GetRootPath 获取项目根路径，请将函数复制到自己的项目中使用，不能直接调用！
 func (obj *Nepal)GetRootPath() (root string) {
 	_,filename,_,_ := runtime.Caller(0)
 	return path.Dir(filename)
+}
+
+// HttpGet 请求一个URL，返回状态码和响应体
+func (obj *Nepal)HttpGet(url string, client *http.Client) (result string, statusCode int) {
+	if client == nil {
+		client = http.DefaultClient
+		client.Timeout = time.Second*3
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err)
+		return "",0
+	}
+
+	resp,err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return "",0
+	}
+	defer resp.Body.Close()
+
+	b,err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return "",resp.StatusCode
+	}
+	return string(b),resp.StatusCode
+}
+
+// ReadFileToSlice 将文件中的所有非空行读入切片
+func (obj *Nepal)ReadFileToSlice(filepath string) (lines []string) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+	reader := bufio.NewReader(f)
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if string(line) != "" {
+			lines = append(lines, string(line))
+		}
+	}
+	return
+}
+
+// CreateHttpProxyClient 创建一个http代理客户端，代理类型由uri确定，支持http/https/socks5，默认为http
+func (obj *Nepal)CreateHttpProxyClient(uri string, user string, pass string, timeout int) *http.Client {
+	proxy := func(_ *http.Request) (*url.URL, error) {
+		u,err := url.Parse(uri)
+		if user != "" && pass != "" {
+			u.User = url.UserPassword(user, pass)
+		}
+		return u,err
+	}
+
+	return &http.Client{
+		Transport:&http.Transport{Proxy: proxy},
+		Timeout: time.Duration(timeout) * time.Second,
+	}
+}
+
+// InitLog 初始化日志器，设置日志前缀、日志路径，默认添加行号显示
+func (obj *Nepal)InitLog(logPrefix string, logFilePath string)  {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	if logPrefix != "" {
+		log.SetPrefix(logPrefix)
+	}
+
+	if logFilePath != "" {
+		logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.SetOutput(logFile)
+	}
 }
--- nepal_test.go
+++ nepal_test.go
@@ -3,22 +3,8 @@ package cob
 import (
 	"fmt"
 	"testing"
-	"time"
 )
 
 func Test(t *testing.T) {
-	//DNepal.CreateFileServerByGin("C:\\lee\\project\\go\\auto", "/static/", "8888", true)
-	//DNepal.CreateFileServer("C:\\lee\\project\\go\\auto", "/static/", "8888", true)
-	fmt.Println(DNepal.GetRootPath())
-}
-
-func TestNepal_GetRandomString(t *testing.T) {
-	fmt.Println(DNepal.GetRandomString(10))
-}
-
-func TestNepal_GetRandomNum(t *testing.T) {
-	for i:=0;i<20;i++{
-		time.Sleep(10*time.Millisecond)
-		fmt.Printf("%v ", DNepal.GetRandomNum(1,1000))
-	}
+	fmt.Println("hello")
 }
