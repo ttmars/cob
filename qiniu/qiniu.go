2b03791f4fd028af11b67023f5ce190d4cdb62df
ttmars <2292752593@qq.com>
2023-01-02 11:15:36 +0800

    fix

--- qiniu/qiniu.go
+++ qiniu/qiniu.go
@@ -121,3 +121,21 @@ func (obj *Qiniu)PutFile(filepath string, filename string)  {
 	}
 	log.Println("上传成功：", filepath)
 }
+
+// DeleteFile 删除bucket中的文件
+func (obj *Qiniu)DeleteFile(filename string) error {
+	mac := qbox.NewMac(obj.AccessKey, obj.SecretKey)
+	cfg := storage.Config{
+		// 是否使用https域名进行资源管理
+		UseHTTPS: true,
+	}
+	// 指定空间所在的区域，如果不指定将自动探测
+	// 如果没有特殊需求，默认不需要指定
+	cfg.Zone=obj.Zone
+	bucketManager := storage.NewBucketManager(mac, &cfg)
+
+	bucket := obj.Bucket
+	key := filename
+	err := bucketManager.Delete(bucket, key)
+	return err
+}
